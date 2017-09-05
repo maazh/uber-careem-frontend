56c84481234dbb112d0dbd207db223e2377588d1
Maaz Hasan <maazhassan2@gmail.com>
2017-09-05 14:47:44 +0500

    Final Project changes 4

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+            /* Set the width of the side navigation to 250px and the left margin of the page content to 250px. Help taken
+            from the internet to solve.*/
+            function openNav() {
+                document.getElementById("mySidenav").style.width = "250px";
+                document.getElementById("main").style.marginLeft = "250px";
+            }
+
+            /* Set the width of the side navigation to 0 and the left margin of the page content to 0. Help taken
+            from the internet to solve.*/
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+                document.getElementById("main").style.marginLeft = "0";
+            }
